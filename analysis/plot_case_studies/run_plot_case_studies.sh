0a77266ac49231dd980b280da1955a0ee654713b
fafafoo <760213960@qq.com>
2026-08-17 23:48:07 +0800

    Delete run_plot_case_studies.sh

--- analysis/plot_case_studies/run_plot_case_studies.sh
+++
@@ -1,7 +0,0 @@
-python -u analysis\plot_case_studies\step0_extract_retest_data.py
-python -u analysis\plot_case_studies\step1_generate_prediction_matrix.py
-python -u analysis\plot_case_studies\step2_compute_topk_validation.py
-python -u analysis\plot_case_studies\step3_extract_top50.py
-python -u analysis\plot_case_studies\step4_map_external_validation.py
-python -u analysis\plot_case_studies\step5_target_recommendation.py
-python -u analysis\plot_case_studies\step6_plot_figure7.py
